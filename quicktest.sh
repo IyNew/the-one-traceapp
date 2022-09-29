3375610724c4d08e3dcdb34b516b5c3c5dd53500
megadonny <33389261+megadonny@users.noreply.github.com>
2022-09-29 11:03:24 -0400

    quicktest script update

--- quicktest.sh
+++ quicktest.sh
@@ -2,18 +2,19 @@ settingpath=./settingfiles
 # pace=0.2
 # interval=100
 # numofnodes=500
-endtime=500
+endtime=5000
 # istracing=false
 # numofTA=1
 
-for pace in 1.0
+for pace in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
-    for interval in 50 
+    for interval in 100 500 1000
     do
-        for numofnodes in 250
+        for numofnodes in 500
         do
-            for numofTA in 50
+            for TARate in 0.1 0.2 0.3 0.4 0.5
             do
+                numofTA=$(echo "$numofnodes*$TARate" | bc)
                 for istracing in 0 1 2
                 do
                     testname="Test-"$numofnodes"nodes-"$numofTA"TA-"$endtime"end-"$interval"-"$pace"-"$istracing"tracemode"
@@ -32,13 +33,15 @@ do
 
                     rm $settingpath/$testname.txt.bk
 
+                    # clear the setting files with arg "clear"
+                    if [ "$1" = "clear" ]; then
+                        rm $settingpath/$testname.txt
+                    fi
                     ./one.sh -b 1 $settingpath/$testname.txt
                 done
             done
         done
     done
 done
-
-
 
 curl -X GET https://api.day.app/i5xtQapPZVuE2gfQLvbZD4/completed
