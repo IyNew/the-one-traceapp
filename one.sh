9bacad1594f8c0284d80d3ec06c5622f2c8f5e8b
megadonny <33389261+megadonny@users.noreply.github.com>
2022-10-29 03:57:27 -0400

    more memory to run

--- one.sh
+++ one.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-java -Xmx512M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx2048M -cp target:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
 
 # send a get request to the notification server
 # curl -X GET https://api.day.app/i5xtQapPZVuE2gfQLvbZD4/completed
